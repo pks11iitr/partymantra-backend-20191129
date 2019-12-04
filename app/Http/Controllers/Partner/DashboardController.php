293b962ac72de09385b3ae3dc5c1bf0bec479792
pankaj sengar <pks11iitr@gmail.com>
2019-12-04 13:57:47 +0530

    setting rolewise login redirections

---
+++ app/Http/Controllers/Partner/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Partner;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class DashboardController extends Controller
+{
+    public function index(Request $request){
+        echo 'welcome partner';die;
+    }
+}
