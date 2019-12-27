e8f74ae2a4411bf3857d75b1024775672e3e9e94
pankaj sengar <pks11iitr@gmail.com>
2019-12-27 19:20:45 +0530

    added reviews at home page

--- app/Http/Controllers/Customer/Api/CollectionController.php
+++ app/Http/Controllers/Customer/Api/CollectionController.php
@@ -37,7 +37,7 @@ class CollectionController extends Controller
         $events=$collection->event()->with('avgreviews')->where('isactive',true)->where('partneractive', true)->orderBy('priority','asc')->get();
         $i=0;
         foreach($events as $e){
-            $events[$i]->rating=$e->avgreviews[0]->rating;
+            $events[$i]->rating=$e->avgreviews[0]->rating??0;
             $i++;
         }
         return ['events'=>$events];
