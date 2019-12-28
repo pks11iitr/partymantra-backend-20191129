0d61f6ebca1a12ce5443f8336684384d9d03ade7
pankaj sengar <pks11iitr@gmail.com>
2019-12-28 14:49:41 +0530

    change in hash function razorpay

--- app/Services/Payment/RazorPayService.php
+++ app/Services/Payment/RazorPayService.php
@@ -34,7 +34,7 @@ class RazorPayService
     }
 
     public function verifypayment($data){
-        //$generated_signature = hmac_sha256($data['razorpay_order_id'] + "|" + $data['razorpay_payment_id'], $this->api_secret);
+        //$generated_signature = hash_hmac('sha256', $data['razorpay_order_id'] + "|" + $data['razorpay_payment_id'], $this->api_secret);
         return true;
         if ($generated_signature == $data['razorpay_signature']) {
            return true;
