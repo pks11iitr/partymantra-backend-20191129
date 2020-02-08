ba7fdee7a7fe01c9046d70db5a8b475109fa7e96
pankaj sengar <pks11iitr@gmail.com>
2020-02-08 12:47:40 +0530

    change in order detail vendors

--- app/Http/Controllers/Partner/Api/OrderController.php
+++ app/Http/Controllers/Partner/Api/OrderController.php
@@ -120,7 +120,7 @@ class OrderController extends Controller
         if($c->entity instanceof Partner){
             $title=$c->entity->name. ("($c->optional_type)"??'');
             $address=$c->entity->address;
-            $date=date('D,d-M-Y', strtotime($order->date)).' '.$order->time;
+            $date=date('D,d M Y', strtotime($order->date)).'-'.$order->time;
         }else{
             $title=$c->entity->title;
             $date=$c->entity->startdate.'-'.$c->entity->enddate;
